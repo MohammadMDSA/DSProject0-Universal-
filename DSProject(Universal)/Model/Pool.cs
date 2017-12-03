4e09a04e6eb01d219e536e902f4f1f9d730cedf8
Mohammad MDSA <samimd.77@gmail.com>
2017-12-03 11:49:32 +0330

    Added Node and LinkedList class

---
+++ DSProject(Universal)/Model/Pool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSProject_Universal_.Model
+{
+	public abstract class SuperPool
+	{
+	}
+
+	sealed class ServicePool : SuperPool
+	{
+
+	}
+
+	sealed class SubServicePool : SuperPool
+	{
+		
+	}
+}
---
+++ DSProject(Universal)/Util/LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSProject_Universal_.Util
+{
+	class LinkedList<T>
+	{
+		private Node<T> First;
+		private Node<T> Last;
+		public int Length { get; private set; }
+
+		public LinkedList()
+		{
+			this.Last = null;
+			this.First = new Node<T>();
+			this.Length = 0;
+		}
+	}
+
+	class Node<T>
+	{
+		public Node<T> Next { get; private set; }
+		public Node<T> Previous { get; private set; }
+		public T Data { get; private set; }
+
+		public Node()
+		{
+			this.Previous = null;
+			this.Next = null;
+		}
+
+		public Node(T data)
+		{
+			this.Data = data;
+		}
+
+		public Node(T data, Node<T> previous) : this(data)
+		{
+			this.Previous = previous;
+		}
+
+		public Node(T data, Node<T> previous, Node<T> next) : this(data, previous)
+		{
+			this.Next = next;
+		}
+
+		public Node<T> SetNext(Node<T> next)
+		{
+			this.Next = next;
+			return this;
+		}
+
+		public Node<T> SetPrevious(Node<T> previous)
+		{
+			this.Previous = previous;
+			return this;
+		}
+
+		public Node<T> SetData(T data)
+		{
+			this.Data = data;
+			return this;
+		}
+	}
+}
